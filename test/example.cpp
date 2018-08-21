04a4614a7804b9b958eec1a5fe07d4f01b24bcc3
sstefan1 <sstipanovic@s-energize.com>
2018-08-21 13:25:02 +0200

    adding gtest

---
+++ test/example.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+    
+TEST(FirstTest, TrueTest) { EXPECT_TRUE(2 == 1); }
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
